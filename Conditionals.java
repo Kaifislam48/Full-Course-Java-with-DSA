159cbae473a77b4dfa257e8ce4b361aedf6c9d50
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-06-30 15:15:35 +0530

    Add files via upload

---
+++ Conditionals.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Conditionals {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if(age>=18){
+            System.out.println("You are adult.");
+        }else if(age<18 && age>=12){
+            System.out.println("You are teenager.");
+        }
+        else{
+            System.out.println("You are child.");
+        }
+    }
+}
