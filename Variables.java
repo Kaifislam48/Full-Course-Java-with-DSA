2d8d086920dfdafb7153035e2ce60905a6495f1b
Kaif Islam <117916990+Kaifislam48@users.noreply.github.com>
2024-06-30 14:56:21 +0530

    Add files via upload

---
+++ Variables.java
@@ -0,0 +1,20 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a = 10;
+        float b = 10;
+        double c = 10.50;
+        char d = 'a';
+        byte f = 2;
+        short g = 4;
+        boolean h = true;
+        String e = "Hello World!";
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+        }
+}
